f8c77a2ea38dcfa8a9de9093ff115ace1599e826
Alejandro Martinez <mart.perez93@gmail.com>
2025-02-20 13:48:16 +0100

    feat: add ListStarshipsComponent and CommonModule to imports app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { ListStarshipsComponent } from './list-starships/list-starships.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ListStarshipsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
