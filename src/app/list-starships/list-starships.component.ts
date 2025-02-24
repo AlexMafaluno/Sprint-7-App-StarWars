54ec6413e12eecdde331e718e5bfe2742e6f3e88
Alejandro Martinez <mart.perez93@gmail.com>
2025-02-24 12:43:40 +0100

    improve ships list

--- src/app/list-starships/list-starships.component.ts
+++ src/app/list-starships/list-starships.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { SpaceshipService } from '../services/spaceship.service';
 import { CommonModule } from '@angular/common';
+import { ShipCardComponent } from "../ship-card/ship-card.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-list-starships',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './list-starships.component.html',
   styleUrl: './list-starships.component.scss'
 })
@@ -12,13 +14,22 @@ export class ListStarshipsComponent implements OnInit {
   
   private spaceshipService = inject(SpaceshipService);
   spaceships: any[] = [];
-
+  selectedShip: any = null;
+  
   constructor(){}
 
   async ngOnInit() {
     this.spaceships = await this.spaceshipService.getStarShips();
+    console.log(this.spaceships)
+  }
+  
+  selectShip(ship: any) {
+    this.selectedShip = ship;
   }
 
+  closeCard() {
+    this.selectedShip = null;
+  }
 }
 
 
