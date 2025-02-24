90ce8c156b191205abc10d3fbb00916b66416a89
Alejandro Martinez <mart.perez93@gmail.com>
2025-02-24 12:42:32 +0100

    update routing and replace starship list component with router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ListStarshipsComponent } from './list-starships/list-starships.component';
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, ListStarshipsComponent],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ShipCardComponent } from './ship-card/ship-card.component';
+import { ListStarshipsComponent } from './list-starships/list-starships.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: 'ships', component: ListStarshipsComponent },
+{ path: 'ships/:id', component: ShipCardComponent },
+{ path: '', redirectTo: '/ships', pathMatch: 'full' }
+];
