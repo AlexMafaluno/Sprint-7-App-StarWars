88e1c9105c70c7d5bb3f8c49ebbb671e890e3aaf
Alejandro Martinez <mart.perez93@gmail.com>
2025-02-26 09:52:26 +0100

    feat: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { ShipCardComponent } from './ship-card/ship-card.component';
 import { ListStarshipsComponent } from './list-starships/list-starships.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-{ path: 'ships', component: ListStarshipsComponent },
-{ path: 'ships/:id', component: ShipCardComponent },
-{ path: '', redirectTo: '/ships', pathMatch: 'full' }
+  { path: '', component: ListStarshipsComponent },
+  { path: 'ships', component: ListStarshipsComponent },
+  { path: 'ships/:id', component: ShipCardComponent },
+  { path: '**', redirectTo: '/' }, // Redirige a Home si la ruta no existe
 ];
