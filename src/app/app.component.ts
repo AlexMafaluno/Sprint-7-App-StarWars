aad30cec962f1be768c561ba1b3e0e5fa567d01a
Alejandro Martinez <mart.perez93@gmail.com>
2025-02-28 10:05:08 +0100

    feat: integrate Firebase and add registration component to app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,28 @@ import { CommonModule } from '@angular/common';
 import { ListStarshipsComponent } from './list-starships/list-starships.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ModalComponent } from './modal/modal.component';
+import { addDoc, collection, Firestore } from 'firebase/firestore';
+import { RegisterComponent } from './components/users/register/register.component';
+
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, RouterModule, HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    HeaderComponent,
+    FooterComponent,
+    ReactiveFormsModule,
+    RegisterComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
   title = 'StarWars_app';
-}
+
+  }
+
