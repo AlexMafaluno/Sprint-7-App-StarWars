daefaabe2997b9a0dc6dda0b052bea8b90dda0d7
Alejandro Martinez <mart.perez93@gmail.com>
2025-02-28 10:07:26 +0100

    feat: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { ShipCardComponent } from './ship-card/ship-card.component';
 import { ListStarshipsComponent } from './list-starships/list-starships.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { RegisterComponent } from './components/users/register/register.component';
 
 export const routes: Routes = [
-  { path: '', component: ListStarshipsComponent },
+  { path: '', component: HomeComponent },
   { path: 'ships', component: ListStarshipsComponent },
   { path: 'ships/:id', component: ShipCardComponent },
-  { path: '**', redirectTo: '/' }, // Redirige a Home si la ruta no existe
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/' } // Redirige a Home si la ruta no existe
 ];
