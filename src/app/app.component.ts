80231b191d3504a35e860db3725762e594b9fa95
Alejandro Martinez <mart.perez93@gmail.com>
2025-02-28 14:07:53 +0100

    feat: delete import Modal in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { ListStarshipsComponent } from './list-starships/list-starships.componen
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ModalComponent } from './modal/modal.component';
 import { addDoc, collection, Firestore } from 'firebase/firestore';
 import { RegisterComponent } from './components/users/register/register.component';
 
