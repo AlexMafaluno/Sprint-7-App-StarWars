c68a566ee9726e781fa4c1c19aedca195995008c
Alejandro Martinez <mart.perez93@gmail.com>
2025-02-28 15:11:10 +0100

    feat: add AuthGuard to ships route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,11 @@ import { HomeComponent } from './components/home/home.component';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/users/login/login.component';
 import { RegisterComponent } from './components/users/register/register.component';
+import { AuthGuard} from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'ships', component: ListStarshipsComponent },
+  { path: 'ships', component: ListStarshipsComponent, canActivate: [AuthGuard] },
   { path: 'ships/:id', component: ShipCardComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
