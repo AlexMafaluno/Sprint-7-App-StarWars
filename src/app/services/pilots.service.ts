468ee45ad89a920e246ac0e7aa714f6b9cc03a09
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-05 18:11:42 +0100

    feat: add PilotsService for fetching and transforming pilot data from API

---
+++ src/app/services/pilots.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Pilot } from '../interfaces/pilot';
+import { SpaceshipService } from './spaceship.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PilotsService {
+  static fetchPilots() {
+    throw new Error('Method not implemented.');
+  }
+  private pilotUrl: string = 'https://swapi.dev/api/people';
+  
+  async fetchPilots(pilots: any): Promise<Pilot[]> {
+    try {
+      const response = await fetch(this.pilotUrl, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Error en la respuesta de l'API");
+      }
+      const data = await response.json();
+      return data.results;
+    } catch (error) {
+      console.error('Ha habido un error:', error);
+      throw error;
+    }
+  };
+ 
+  transformPilots(data: any): Pilot[] {
+    return data.results.map((pilot: any) => ({
+      name: pilot.name,
+      image: pilot.url
+    }));
+  };
+
+
+
+
+  
+}
