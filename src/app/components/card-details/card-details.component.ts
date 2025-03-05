1109fc4fcd6fd20fceba6bdf280134287de5c87d
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-05 18:11:23 +0100

    feat: update card details component to display pilot information dynamically

--- src/app/components/card-details/card-details.component.ts
+++ src/app/components/card-details/card-details.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { PilotsService } from '../../services/pilots.service';
+import { SpaceshipService } from '../../services/spaceship.service';
 
 @Component({
   selector: 'app-card-details',
@@ -6,6 +8,28 @@ import { Component } from '@angular/core';
   templateUrl: './card-details.component.html',
   styleUrl: './card-details.component.scss'
 })
-export class CardDetailsComponent {
 
+export class CardDetailsComponent{
+
+  @Input()pilots: any[] = [];
+/*
+  private pilotsService = inject(PilotsService);
+private spaceshipService = inject(SpaceshipService);
+
+  async ngOnInit() {
+    if (!this.pilots || this.pilots.length === 0) { 
+      await this.loadPilots(); // Solo cargar si el padre no envió datos
+    }
+  }
+
+  async loadPilots() {
+    try {
+      const data = await this.pilotsService.fetchPilots(); // Assuming getPilotsData() fetches the required data
+      this.pilots = this.pilotsService.transformPilots(data);
+      console.log('Pilots loaded:', this.pilots);
+    } catch (error) {
+      console.error('Error loading pilots:', error);
+    }
+  }
+*/
 }
