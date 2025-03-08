7a7da19bd6f11b2aadb21076f4f7d69dc4b73e4d
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-08 10:15:07 +0100

    feat: add validation for input

--- src/app/components/users/login/login.component.ts
+++ src/app/components/users/login/login.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
