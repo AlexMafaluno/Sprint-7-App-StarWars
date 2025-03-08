2d3c202186bc2e4c70e88d85bab4772caa7c33cb
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-08 16:48:17 +0100

    fix: Fix formatting of routes in app.routes.ts to improve readability

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/users/login/login.component';
 import { RegisterComponent } from './components/users/register/register.component';
 import { AuthGuard } from './guards/auth.guard';
-import { canActivate } from '@angular/fire/auth-guard';
+
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
-  { path: 'ships', component: ListStarshipsComponent, canMatch:[AuthGuard] },
+  { path: 'ships', component: ListStarshipsComponent, canMatch: [AuthGuard] },
   { path: 'ships/:id', component: ShipCardComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: '**', redirectTo: '/' } // Redirige a Home si la ruta no existe
+  { path: '**', redirectTo: '/' }, 
 ];
