5626853f05017e55848aca13bf03596daa22b2e7
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-08 16:10:59 +0100

    feat: Refactor login and register components to improve code readability and error handling

--- src/app/components/users/login/login.component.ts
+++ src/app/components/users/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -8,72 +13,61 @@ import { CommonModule } from '@angular/common';
   selector: 'app-login',
   imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
- 
-formLogin: FormGroup;
-private authService = inject(AuthService);
-private router = inject(Router);
+  formLogin: FormGroup;
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
- constructor(private fb: FormBuilder) {
-     this.formLogin = this.fb.group({
-       mail: ['', [Validators.required, Validators.email]],
-       password: ['', [Validators.required, Validators.minLength(6)]],
-     });
-   }
+  constructor(private fb: FormBuilder) {
+    this.formLogin = this.fb.group({
+      mail: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 
-async loginEmailPassword() {
+  async loginEmailPassword() {
     if (this.formLogin.invalid) {
       console.log('Formulario invalido');
       return;
     }
-      const response = this.formLogin.value;
-      const loginEmail: string = response.mail || '';
-      const loginPassword: string = response.password || '';
-
-      console.log(response); //envio al back y me devuelve la respuesta
-      console.log(loginEmail);
-      console.log(loginPassword);
-      try {
-        const userCredential = await this.authService.login(
-          loginEmail,
-          loginPassword
-        );
-        console.log(userCredential.user);
-        this.router.navigate(['/ships']);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    async logout() {
-      try {
-        await this.authService.logout();
-        console.log('Sesión cerrada correctamente');
-        this.router.navigate(['/register']);
-      } catch (error) {
-        console.error('Error al cerrar sesión:', error);
-      }
+    const { mail, password } = this.formLogin.value;
+    try {
+      this.authService.login(mail, password);
+      this.router.navigate(['/ships']);
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    loginGoogle() {
-      this.authService.loginWithGoogle()
-      .then( response => {
+  async logout() {
+    try {
+      await this.authService.logout();
+      console.log('Sesión cerrada correctamente');
+      this.router.navigate(['/register']);
+    } catch (error) {
+      console.error('Error al cerrar sesión:', error);
+    }
+  }
+
+  loginGoogle() {
+    this.authService
+      .loginWithGoogle()
+      .then((response) => {
         console.log(response);
         this.router.navigate(['/ships']);
       })
-      .catch( error => {
+      .catch((error) => {
         console.error('Error al loguear con Google:', error);
       });
+  }
 
-}
-
-
-isInvalid(field: string): boolean {
-  return (
-    this.formLogin.controls[field].invalid &&
-    (this.formLogin.controls[field].dirty || this.formLogin.controls[field].touched)
-  );
-}
+  isInvalid(field: string): boolean {
+    return (
+      this.formLogin.controls[field].invalid &&
+      (this.formLogin.controls[field].dirty ||
+        this.formLogin.controls[field].touched)
+    );
+  }
 }
--- src/app/components/users/register/register.component.ts
+++ src/app/components/users/register/register.component.ts
@@ -1,19 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { UsersService } from '../../../services/users.service';
-import { catchError } from 'rxjs';
 import {
-  connectAuthEmulator,
-  createUserWithEmailAndPassword,
-  getAuth,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
-} from 'firebase/auth';
-import { initializeApp } from 'firebase/app';
-import { environment } from '../../../../environments/environment';
-import { Auth } from '@angular/fire/auth';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -25,64 +17,36 @@ import { CommonModule } from '@angular/common';
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  data = '';
   formReg: FormGroup;
   private authService = inject(AuthService);
-  private auth = inject(Auth);
   private router = inject(Router);
 
-  
   constructor(private fb: FormBuilder) {
     this.formReg = this.fb.group({
       mail: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
-
   }
 
-  
-  async createAccount () {
+  async createAccount() {
     if (this.formReg.invalid) {
-      console.log('Formulario invalido');
+      console.error('Formulario invalido');
       return;
     }
-    
-      const response = this.formReg.value;
-      const loginEmail: string = response.mail || '';
-      const loginPassword: string = response.password || '';
-
-      console.log(response); //envio al back y me devuelve la respuesta
-      console.log(loginEmail);
-      console.log(loginPassword);
-      try {
-        const userCredential = await this.authService.register(
-          loginEmail,
-          loginPassword
-        );
-        console.log(userCredential.user);
-        this.router.navigate(['/login']);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-  async monitorAuthState(): Promise<void> {
-    onAuthStateChanged(this.auth, (user) => {
-      if (user) {
-        console.log('logged in');
-      } else {
-        console.log('no user');
-      }
-    });
+    const { mail, password } = this.formReg.value;
+    try {
+      this.authService.register(mail, password);
+      this.router.navigate(['/login']);
+    } catch (error) {
+      console.error(error);
+    }
   }
+
   isInvalid(field: string): boolean {
     return (
       this.formReg.controls[field].invalid &&
-      (this.formReg.controls[field].dirty || this.formReg.controls[field].touched)
+      (this.formReg.controls[field].dirty ||
+        this.formReg.controls[field].touched)
     );
-  }
-
-  ngOnInit() {
-    this.monitorAuthState();
   }
 }
