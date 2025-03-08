4ca25a8cd2d9a6d0a1b1aa31bc37136db56a063b
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-08 09:32:54 +0100

    feat: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './components/home/home.component';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/users/login/login.component';
 import { RegisterComponent } from './components/users/register/register.component';
-import { AuthGuard} from './guards/auth.guard';
+import { AuthGuard } from './guards/auth.guard';
+import { canActivate } from '@angular/fire/auth-guard';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'ships', component: ListStarshipsComponent, canActivate: [AuthGuard] },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'ships', component: ListStarshipsComponent, canMatch:[AuthGuard] },
   { path: 'ships/:id', component: ShipCardComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
