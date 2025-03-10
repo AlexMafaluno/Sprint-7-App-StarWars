a6eb8d59c3d5271a40d3b5cd4870e98cf4030c32
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-10 19:01:01 +0100

    feat: add dotenv support for environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,18 @@
+import { config } from 'dotenv';
+
+config();
 
 export const environment  = {
     production: false,
     apiBaseUrl: 'https://swapi.dev/api',
     firebaseConfig :{
-        apiKey: "AIzaSyDhK8gmVsxNXt9XpRqMpBcgchRFilV6Avk",
-        authDomain: "starwarsapp-52865.firebaseapp.com",
-        projectId: "starwarsapp-52865",
-        storageBucket: "starwarsapp-52865.firebasestorage.app",
-        messagingSenderId: "348253267871",
-        appId: "1:348253267871:web:9069e15175f10c7df53faa",
-        measurementId: "G-T6FX949DYG"
+        apiKey: process.env['FIREBASE_API_KEY'],
+        authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+        projectId: process.env['FIREBASE_PROJECT_ID'],
+        storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+        messagingSenderId:process.env['FIREBASE_MESSAGING_SENDER_ID'],
+        appId: process.env['FIREBASE_APP_ID'],
+        measurementId: process.env['FIREBASE_MEASUREMENT_ID'],
     },
     useEmulator: false 
     };
